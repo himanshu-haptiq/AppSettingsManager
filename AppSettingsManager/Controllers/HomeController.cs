150cf1626c6867ce86d11a283646ecbaac382126
Himanshu Singhal <himanshu.singhal@haptiq.com>
2024-07-18 16:52:38 +0530

    Added different ways to read app settings

--- AppSettingsManager/Controllers/HomeController.cs
+++ AppSettingsManager/Controllers/HomeController.cs
@@ -7,14 +7,32 @@ namespace AppSettingsManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
+            // IConfiguration contains all the configurations from appsettings file
+            ViewBag.SendGridKey = _config.GetValue<string>("SendGridKey");
+
+            // Get appsettings details from a section named twilio
+            // Preferred way to get data from sections
+            ViewBag.TwilioAuthToken = _config.GetValue<string>("Twilio:AuthToken");
+            ViewBag.TwilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
+
+            // Less preferred way for getting keys from appsettings sections
+            // If number of nested sections increase, then it is a pain
+            //ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("AuthToken");
+            //ViewBag.TwilioAccountSid = _config.GetSection("Twilio").GetValue<string>("AccountSid");
+
+            //ViewBag.ThirdLevelSettingValue = _config.GetValue<string>("FirstLevelSetting:SecondLevelSetting:BottomLevelSetting");
+            ViewBag.ThirdLevelSettingValue = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetValue<string>("BottomLevelSetting");
+
             return View();
         }
 
